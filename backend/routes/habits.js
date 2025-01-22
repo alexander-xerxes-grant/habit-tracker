d2092e39fbef8c2addacc7dc8da2dfb13b582695
Alex Grant <alexanderxgrant@gmail.com>
2025-01-22 07:48:22 +0000

    Add PUT route.

--- backend/routes/habits.js
+++ backend/routes/habits.js
@@ -35,4 +35,32 @@ try {
 }
 });
 
+
+// Put a habit
+// This route will update a habit in the database
+// We use the :id parameter to specify which habit to update
+// id will be passed in the URL
+router.post('/:id', async (req, res) => {
+    try {
+        const updatedHabit = await Habit.findByIdAndUpdate(
+            req.params.id,
+            {
+                name: req.body.name
+            },
+            { new: true }
+        );
+
+        // If the habit doesn't exist, return a 404 status code
+        if (!updatedHabit) {
+            return res.status(404).json({ message: 'Habit not found' });
+            }
+        
+        res.json(updatedHabit);
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+        )
+
 module.exports = router;
